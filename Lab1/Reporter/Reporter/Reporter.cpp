a3d5430c33ecbdf2ff2f8b80ccaa8f4b639429f4
Viachaslau Pertseu <slava02007@gmail.com>
2025-12-29 20:42:55 +0300

    Add files via upload

--- Lab1/Reporter/Reporter/Reporter.cpp
+++ Lab1/Reporter/Reporter/Reporter.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip> 
+
 using namespace std;
 
 struct employee {
@@ -13,39 +15,39 @@ int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "rus");
 
     if (argc < 4) {
-        cerr << "Usage: Reporter <input_file> <report_file> <pay_per_hour>" << endl;
         return 1;
     }
 
     string binFile = argv[1];
     string reportFile = argv[2];
-    double payPerHour = stod(argv[3]);
+    double payPerHour = atof(argv[3]);
 
     ifstream fin(binFile, ios::binary);
     if (!fin) {
-        cerr << "Error: cannot open binary file " << binFile << endl;
+        cerr << "Ошибка открытия бинарного файла!" << endl;
         return 1;
     }
 
     ofstream fout(reportFile);
     if (!fout) {
-        cerr << "Error: cannot create report file " << reportFile << endl;
+        cerr << "Ошибка создания файла отчета!" << endl;
         return 1;
     }
 
-    fout << "Отчет по файлу \"" << binFile << "\"\n";
-    fout << "Номер\tИмя\tЧасы\tЗарплата\n";
+    fout << "Отчет по файлу \"" << binFile << "\"" << endl;
+    fout << "Номер\tИмя\tЧасы\tЗарплата" << endl;
 
     employee emp;
-    while (fin.read(reinterpret_cast<char*>(&emp), sizeof(employee))) {
+    fout << fixed << setprecision(2);
+
+    while (fin.read((char*)&emp, sizeof(employee))) {
         double salary = emp.hours * payPerHour;
-        fout << emp.num << "\t" << emp.name << "\t"
-            << emp.hours << "\t" << salary << "\n";
+        fout << emp.num << "\t" << emp.name << "\t" << emp.hours << "\t" << salary << endl;
     }
 
     fin.close();
     fout.close();
 
-    cout << "Отчет сохранён в файл " << reportFile << endl;
+    cout << "Отчет сформирован в файле: " << reportFile << endl;
     return 0;
 }
