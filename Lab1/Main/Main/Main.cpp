b988c12229d480c2eaf799a037b376db4dfe3e54
Viachaslau Pertseu <slava02007@gmail.com>
2025-12-29 20:38:44 +0300

    Add files via upload

--- Lab1/Main/Main/Main.cpp
+++ Lab1/Main/Main/Main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 struct employee {
@@ -10,99 +12,84 @@ struct employee {
     double hours;
 };
 
+void runProcess(wstring cmdLine) {
+    STARTUPINFOW si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
+    vector<wchar_t> cmdBuf(cmdLine.begin(), cmdLine.end());
+    cmdBuf.push_back(0); 
+
+    if (!CreateProcessW(
+        NULL,
+        cmdBuf.data(),
+        NULL, NULL, FALSE,
+        CREATE_NEW_CONSOLE, 
+        NULL, NULL,
+        &si, &pi
+    )) {
+        cerr << "Ошибка при запуске процесса! Код ошибки: " << GetLastError() << endl;
+        return;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE); 
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    
     string binFile;
     int n;
+    cout << "--- Подготовка данных ---" << endl;
     cout << "Введите имя бинарного файла: ";
     cin >> binFile;
     cout << "Введите количество сотрудников: ";
     cin >> n;
 
-   
-    string creatorCmd = "Creator.exe " + binFile + " " + to_string(n);
-    wstring wCreatorCmd(creatorCmd.begin(), creatorCmd.end());
+    wstring wCreatorCmd = L"Creator.exe " + wstring(binFile.begin(), binFile.end()) + L" " + to_wstring(n);
+    runProcess(wCreatorCmd);
 
-    STARTUPINFOW si = { sizeof(si) };
-    PROCESS_INFORMATION pi;
-
-    if (!CreateProcessW(
-        NULL,
-        (LPWSTR)wCreatorCmd.c_str(), 
-        NULL, NULL, FALSE,
-        0, NULL, NULL,
-        &si, &pi
-    )) {
-        cerr << "Ошибка запуска Creator\n";
-        return 1;
-    }
-
-    
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-
-   
-    cout << "\nСодержимое бинарного файла:\n";
+    cout << "\nСодержимое бинарного файла:" << endl;
     ifstream fin(binFile, ios::binary);
     if (!fin) {
-        cerr << "Ошибка открытия бинарного файла!\n";
-        return 1;
+        cerr << "Ошибка: не удалось открыть созданный файл " << binFile << endl;
+    }
+    else {
+        employee emp;
+        while (fin.read((char*)&emp, sizeof(employee))) {
+            cout << "ID: " << emp.num << "\tИмя: " << emp.name << "\tЧасы: " << emp.hours << endl;
+        }
+        fin.close();
     }
 
-    employee emp;
-    while (fin.read((char*)&emp, sizeof(employee))) {
-        cout << emp.num << "\t" << emp.name << "\t" << emp.hours << endl;
-    }
-    fin.close();
-
-    
     string reportFile;
     double pay;
-    cout << "\nВведите имя файла отчета: ";
+    cout << "\n--- Формирование отчета ---" << endl;
+    cout << "Введите имя файла отчета: ";
     cin >> reportFile;
     cout << "Введите оплату за час: ";
     cin >> pay;
 
-   
-    string reporterCmd = "Reporter.exe " + binFile + " " + reportFile + " " + to_string(pay);
-    wstring wReporterCmd(reporterCmd.begin(), reporterCmd.end());
+    wstring wReporterCmd = L"Reporter.exe " + wstring(binFile.begin(), binFile.end()) +
+        L" " + wstring(reportFile.begin(), reportFile.end()) +
+        L" " + to_wstring(pay);
+    runProcess(wReporterCmd);
 
-    STARTUPINFOW si2 = { sizeof(si2) };
-    PROCESS_INFORMATION pi2;
-
-    if (!CreateProcessW(
-        NULL,
-        (LPWSTR)wReporterCmd.c_str(),
-        NULL, NULL, FALSE,
-        0, NULL, NULL,
-        &si2, &pi2
-    )) {
-        cerr << "Ошибка запуска Reporter\n";
-        return 1;
-    }
-
-   
-    WaitForSingleObject(pi2.hProcess, INFINITE);
-    CloseHandle(pi2.hProcess);
-    CloseHandle(pi2.hThread);
-
-   
-    cout << "\nСодержимое отчета:\n";
+    cout << "\nИтоговый отчет:" << endl;
     ifstream fr(reportFile);
     if (!fr) {
-        cerr << "Ошибка открытия файла отчёта!\n";
-        return 1;
+        cerr << "Ошибка: файл отчета не был создан!" << endl;
+    }
+    else {
+        string line;
+        while (getline(fr, line)) {
+            cout << line << endl;
+        }
+        fr.close();
     }
 
-    string line;
-    while (getline(fr, line)) {
-        cout << line << endl;
-    }
-    fr.close();
-
-    cout << "\nРабота программы завершена.\n";
+    cout << "\nПрограмма Main завершила работу." << endl;
+    system("pause");
     return 0;
 }
